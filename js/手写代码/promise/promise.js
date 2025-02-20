3e8378ee220eb5cbec96c0c904de4594373094ca
qianxunyimeng <460960620@qq.com>
2025-02-20 17:47:43 +0800

    补充

--- js/手写代码/promise/promise.js
+++ js/手写代码/promise/promise.js
@@ -28,6 +28,57 @@ class MyPromise {
     this.#changeState(REJECTED,reason)
   }
 
+  // promise状态改变
+  #changeState (state, result) {
+    // 状态只会改变一次，如果已经改变直接返回
+    if (this.#PromiseState !== PENDING) {
+      return
+    }
+    this.#PromiseState = state
+    this.#PromiseResult = result
+    this.#run()
+  }
+
+  #runOne (callback, resolve, reject) {
+    this.#runMicroTask(() => {
+      if (typeof callback !== "function") {
+        // 回调不是函数，发生穿透
+        const settled = this.#PromiseState === FULFILLED ? resolve : reject
+        settled(this.#PromiseResult)
+      } else {
+        // 回调是函数就执行函数
+        try {
+          const data = callback(this.#PromiseResult)
+          // 如果回调函数的返回结果是promise
+          if (this.#isPromiseLike(data)) {
+            data.then(resolve, reject)
+          } else {
+            resolve(data)
+          }
+        } catch (error) {
+          reject(error)
+        }
+      }
+    })
+  }
+  #run () {
+    if (this.#PromiseState === PENDING) {
+      return
+    }
+
+    while (this.#callbackHandler.length) {
+      // 弹出第一项，执行
+      const { onFulfilled, onRejected, resolve, reject } = this.#callbackHandler.shift()
+      if (this.#PromiseState === FULFILLED) {
+        this.#runOne(onFulfilled, resolve, reject)
+      } else if (this.#PromiseState === REJECTED) {
+        this.#runOne(onRejected, resolve, reject)
+      } else {
+
+      }
+    }
+  }
+
   then (onFulfilled,onRejected) { 
     const promise2 = new MyPromise((resolve,reject) => { 
       this.#callbackHandler.push({
@@ -93,56 +144,9 @@ class MyPromise {
     }
   }
 
-  // promise状态改变
-  #changeState (state, result) { 
-    // 状态只会改变一次，如果已经改变直接返回
-    if (this.#PromiseState !== PENDING) {
-      return
-    }
-    this.#PromiseState = state
-    this.#PromiseResult = result
-    this.#run()
-  }
+  
 
-  #runOne (callback, resolve, reject) { 
-    this.#runMicroTask(() => { 
-      if (typeof callback !== "function") {
-        // 回调不是函数，发生穿透
-        const settled = this.#PromiseState === FULFILLED ? resolve : reject
-        settled(this.#PromiseResult)
-      } else {
-        // 回调是函数就执行函数
-        try {
-          const data = callback(this.#PromiseResult)
-          // 如果回调函数的返回结果是promise
-          if (this.#isPromiseLike(data)) {
-            data.then(resolve, reject)
-          } else {
-            resolve(data)
-          }
-        } catch (error) {
-          reject(error)
-        }
-      }
-    })
-  }
-  #run () { 
-    if (this.#PromiseState === PENDING) {
-      return
-    }
-
-    while (this.#callbackHandler.length) { 
-      // 弹出第一项，执行
-      const { onFulfilled, onRejected, resolve, reject } = this.#callbackHandler.shift()
-      if (this.#PromiseState === FULFILLED) {
-        this.#runOne(onFulfilled,resolve,reject)
-      } else if (this.#PromiseState === REJECTED) {
-        this.#runOne(onRejected,resolve,reject)
-      } else {
-
-      }
-    }
-  }
+ 
 
   #isPromiseLike (fn) {
     if (fn != null && (typeof fn === "function" || typeof fn === "object") && typeof fn.then === "function") {
@@ -221,14 +225,15 @@ async function mian () {
 //mian()
 
 function mayBeSyncOrAsync () {
-  if (Math.random() > 0.5) {
-    return 'Sync value';
-  } else {
-    return MyPromise.resolve('Async value');
-  }
+  // if (Math.random() > 0.5) {
+  //   return 'Sync value';
+  // } else {
+  //   return MyPromise.resolve('Async value');
+  // }
+  return MyPromise.resolve('Async value');
 }
 
-MyPromise.try(() => mayBeSyncOrAsync())
+MyPromise.try(mayBeSyncOrAsync)
   .then(result => { 
     console.log('Result:', result)
     return result
